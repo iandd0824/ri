c08d05888815da52b9a46b35e669ed07476b58e9
Danny Fu <iandd0824@gmail.com>
2015-08-08 22:39:40 -0700

    add Alamofire framework

--- ios/ri/ri/LoginViewController.swift
+++ ios/ri/ri/LoginViewController.swift
@@ -9,11 +9,15 @@
 import Foundation
 import UIKit
 
+import Alamofire
+
+
 
 class LoginViewController: UIViewController,UITextFieldDelegate {
     
-    @IBOutlet var txtUsername : UITextField!
-    @IBOutlet var txtPassword : UITextField!
+
+    @IBOutlet var txtUsername: UITextField!
+    @IBOutlet var txtPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +30,26 @@ class LoginViewController: UIViewController,UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signinTapped(sender: AnyObject) {
+        
+        var username:NSString = txtUsername.text
+        var password:NSString = txtPassword.text
+        
+        let user2 = "admin"
+        let password2 = "admin"
+        
+        let plainString = "\(username):\(password)" as NSString
+        let plainData = plainString.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64String = plainData?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+        
+        Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = ["Authorization": "Basic " + base64String!]
+        
+        Alamofire.request(.GET, "http://127.0.0.1:8000/snippets/?format=json")
+            .response {(request, response, _, error) in
+                println(response)}
+            .responseJSON { (request, response, JSON, error) in
+                println(JSON)}
+        
+    }
     
 }
